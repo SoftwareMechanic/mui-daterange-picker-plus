b25918a0a7fe2390ddd4de511db083fdca20c274
SoftwareMechanic <baruchmanigrasso@gmail.com>
2025-04-07 22:24:09 +0200

    Update PickerModal.tsx

--- src/PickerModal.tsx
+++ src/PickerModal.tsx
@@ -1,14 +1,19 @@
 import { DateRangePicker } from "./DateRangePicker";
 import type { PickerModalProps } from "./types";
-import { Dialog, useTheme, Popover, useMediaQuery } from "@mui/material";
+import { 
+  Dialog, 
+  //useTheme, 
+  Popover, 
+  useMediaQuery 
+} from "@mui/material";
 
 export const PickerModal = ({
   modalProps,
   customProps,
   ...dateRangePickerProps
 }: PickerModalProps) => {
-  const theme = useTheme();
-  const isMobileView = useMediaQuery(theme.breakpoints.down("md"));
+  // const theme = useTheme();
+  // const isMobileView = useMediaQuery(theme.breakpoints.down("md"));
 
   if (isMobileView) {
     const { open, onClose } = modalProps;
